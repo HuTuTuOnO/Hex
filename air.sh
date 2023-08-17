6f2d6adf3f2aa694fdf01e0890348340717131c8
Colby Cleek <122086569+HuTuTuOnO@users.noreply.github.com>
2023-08-18 02:33:31 +0800

    Create air.sh

---
+++ air.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+[[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] 请使用root用户来执行脚本!" && exit 1
