c856a643671cd5b0c4a1353f4adcc8297150c116
Colby Cleek <122086569+HuTuTuOnO@users.noreply.github.com>
2023-08-18 02:35:27 +0800

    Update air.sh

--- air.sh
+++ air.sh
@@ -8,3 +8,11 @@ yellow='\033[0;33m'
 plain='\033[0m'
 
 [[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] 请使用root用户来执行脚本!" && exit 1
+
+hello(){
+    echo ""
+    echo -e "${yellow}VMSSR AIR脚本${plain}"
+    echo -e "${yellow}支持系统:  CentOS 6+, Debian8+, Ubuntu16+${plain}"
+    echo ""
+}
+
