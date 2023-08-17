27a7081156567ebcfefb8150a16bfbb2967ed40c
Colby Cleek <122086569+HuTuTuOnO@users.noreply.github.com>
2023-08-18 02:48:46 +0800

    Update air.sh

--- air.sh
+++ air.sh
@@ -1,36 +1,10 @@
-#!/usr/bin/env bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
+#!/bin/bash
+# VMSSR 多功能机场脚本
+# Author: HuTuTu<https://www.vmssr.co/>
 
-red='\033[0;31m'
-green='\033[0;32m'
-yellow='\033[0;33m'
-plain='\033[0m'
-
-[[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] 请使用root用户来执行脚本!" && exit 1
-
-if [[ $# = 1 ]];then
-    key="$1"
-    case $key in
-        -i|--install)
-        help
-        ;;
-        -h|--help|*)
-        help
-        ;;
-    esac
-else
-    help
-fi
-
-hello(){
-    echo ""
-    echo -e "${yellow}VMSSR AIR脚本${plain}"
-    echo -e "${yellow}支持系统:  CentOS 6+, Debian8+, Ubuntu16+${plain}"
-    echo ""
-}
-
-help(){
-    hello
-}
+RED="\033[31m"      # Error message
+GREEN="\033[32m"    # Success message
+YELLOW="\033[33m"   # Warning message
+BLUE="\033[36m"     # Info message
+PLAIN='\033[0m'     # Plain message
 
