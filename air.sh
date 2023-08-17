1d761f728657abf98a8291db96246567e7595ec2
Colby Cleek <122086569+HuTuTuOnO@users.noreply.github.com>
2023-08-18 03:00:39 +0800

    Update air.sh

--- air.sh
+++ air.sh
@@ -8,6 +8,41 @@ YELLOW="\033[33m"   # Warning message
 BLUE="\033[36m"     # Info message
 PLAIN='\033[0m'     # Plain message
 
+checkSystem() {
+    result=$(id | awk '{print $1}')
+    if [[ $result != "uid=0(root)" ]]; then
+        colorEcho $RED " 请以root身份执行该脚本"
+        exit 1
+    fi
+
+    res=`which yum 2>/dev/null`
+    if [[ "$?" != "0" ]]; then
+        res=`which apt 2>/dev/null`
+        if [[ "$?" != "0" ]]; then
+            colorEcho $RED " 不受支持的Linux系统"
+            exit 1
+        fi
+        PMT="apt"
+        CMD_INSTALL="apt install -y "
+        CMD_REMOVE="apt remove -y "
+        CMD_UPGRADE="apt update; apt upgrade -y; apt autoremove -y"
+    else
+        PMT="yum"
+        CMD_INSTALL="yum install -y "
+        CMD_REMOVE="yum remove -y "
+        CMD_UPGRADE="yum update -y"
+    fi
+    res=`which systemctl 2>/dev/null`
+    if [[ "$?" != "0" ]]; then
+        colorEcho $RED " 系统版本过低，请升级到最新版本"
+        exit 1
+    fi
+}
+
+colorEcho() {
+    echo -e "${1}${@:2}${PLAIN}"
+}
+
 menu() {
     clear
     echo "#############################################################"
@@ -17,4 +52,18 @@ menu() {
     echo "#############################################################"
 
     echo -e "  ${GREEN}1.${PLAIN}   替换XrayR DNS解锁地址"
+    echo " -------------"
+    echo -e "  ${GREEN}0.${PLAIN}   退出"
+    read -p " 请选择操作[0-17]：" answer
+    case $answer in
+        0)
+            exit 0
+            ;;
+        *)
+            colorEcho $RED " 请选择正确的操作！"
+            exit 1
+            ;;
+    esac
+}
+
 }
