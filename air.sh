7696a69e54f756d6267c8c7a4a784f133a68c741
Colby Cleek <122086569+HuTuTuOnO@users.noreply.github.com>
2023-08-18 02:53:21 +0800

    Update air.sh

--- air.sh
+++ air.sh
@@ -8,3 +8,13 @@ YELLOW="\033[33m"   # Warning message
 BLUE="\033[36m"     # Info message
 PLAIN='\033[0m'     # Plain message
 
+menu() {
+    clear
+    echo "#############################################################"
+    echo -e "#                   ${RED}VMSSR 多功能机场脚本${PLAIN}        #"
+    echo -e "# ${GREEN}作者${PLAIN}: HuTuTu                           #"
+    echo -e "# ${GREEN}网址${PLAIN}: https://www.vmssr.co                 #"
+    echo "#############################################################"
+
+    echo -e "  ${GREEN}1.${PLAIN}   替换XrayR DNS解锁地址"
+}
