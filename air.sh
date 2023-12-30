072e3085f3a1af15b56c01503cdbd8720705228a
HuTuTu <122086569+HuTuTuOnO@users.noreply.github.com>
2023-12-30 15:44:16 +0800

    Update air.sh

--- air.sh
+++ air.sh
@@ -13,21 +13,21 @@ colorEcho() {
 }
 
 modifyXrayRConfig() {
-    echo -e "> 替换XrayR-Config配置"
+    echo -e "> 替换XrayR-CONFIG配置"
         if [ $# -lt 2 ]; then
-        read -ep "请输入原DNS地址: " oldText &&
-        read -ep "请输入新DNS地址: " oldText &&
-        if [[ -z "${oldDnsAddress}" || -z "${newDnsAddress}" ]]; then
+        read -ep "请输入原文本: " oldText &&
+        read -ep "请输入新文本: " newText &&
+        if [[ -z "${oldText}" || -z "${newText}" ]]; then
             echo -e "${red}所有选项都不能为空${plain}"
             beforeShowMenu
             return 1
         fi
     else
-        oldDnsAddress=$1
-        newDnsAddress=$2
+        oldText=$1
+        newText=$2
     fi
     
-    sed -i "s/$oldDnsAddress/$newDnsAddress/" /etc/XrayR/dns.json
+    sed -i "s/$oldText/$newText/" /etc/XrayR/config.yml
 
     echo -e "替换XrayRDNS解锁地址 ${green}修改成功，请稍等重启生效${plain}"
 
@@ -38,22 +38,22 @@ modifyXrayRConfig() {
     fi
 }
 
-modifyXrayRDnsAddress() {
-    echo -e "> 替换XrayRDNS解锁地址"
+modifyXrayRDns() {
+    echo -e "> 替换XrayR-DNS配置"
         if [ $# -lt 2 ]; then
-        read -ep "请输入原DNS地址: " oldDnsAddress &&
-        read -ep "请输入新DNS地址: " newDnsAddress &&
-        if [[ -z "${oldDnsAddress}" || -z "${newDnsAddress}" ]]; then
+        read -ep "请输入原文本: " oldText &&
+        read -ep "请输入新文本: " newText &&
+        if [[ -z "${oldText}" || -z "${newText}" ]]; then
             echo -e "${red}所有选项都不能为空${plain}"
             beforeShowMenu
             return 1
         fi
     else
-        oldDnsAddress=$1
-        newDnsAddress=$2
+        oldText=$1
+        newText=$2
     fi
     
-    sed -i "s/$oldDnsAddress/$newDnsAddress/" /etc/XrayR/dns.json
+    sed -i "s/$oldText/$newText/" /etc/XrayR/dns.json
 
     echo -e "替换XrayRDNS解锁地址 ${green}修改成功，请稍等重启生效${plain}"
 
@@ -77,13 +77,18 @@ showMenu() {
     echo -e "# ${GREEN}网址${PLAIN}: https://www.hu.tu"
     echo "#############################################################"
 
-    echo -e "  ${GREEN}1.${PLAIN}   替换XrayR DNS解锁地址"
+    echo -e "  ${GREEN}1.${PLAIN}   替换XrayR-Config配置"
+    echo -e "  ${GREEN}2.${PLAIN}   替换XrayR-DNS配置"
     echo " -------------"
     echo -e "  ${GREEN}0.${PLAIN}   退出"
     read -p " 请选择操作[0-17]：" answer
     case $answer in
         1)
-            modifyXrayRDnsAddress 0
+            modifyXrayRConfig 0
+            exit 0
+            ;;
+        2)
+            modifyXrayRDns 0
             exit 0
             ;;
         *)
@@ -95,12 +100,20 @@ showMenu() {
 
 if [[ $# > 0 ]]; then
     case $1 in
-        "modifyXrayRDnsAddress")
+        "modifyXrayRConfig")
             shift
             if [ $# -ge 2 ]; then
-                modifyXrayRDnsAddress "$@"
+                modifyXrayRConfig "$@"
             else
-                modifyXrayRDnsAddress 0
+                modifyXrayRConfig 0
+            fi
+        ;;
+        "modifyXrayRDns")
+            shift
+            if [ $# -ge 2 ]; then
+                modifyXrayRDns "$@"
+            else
+                modifyXrayRDns 0
             fi
         ;;
         *)
